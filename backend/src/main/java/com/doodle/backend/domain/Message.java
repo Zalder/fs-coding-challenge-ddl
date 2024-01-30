afe8375ad3ab186c0dafd8c4fd5ed0b95217464e
Alexandre Vanier <alexandre.vanier2@gmail.com>
2024-01-30 02:43:28 +0100

    Setup docker with backend service and database

--- backend/src/main/java/com/doodle/backend/domain/Message.java
+++ backend/src/main/java/com/doodle/backend/domain/Message.java
@@ -1,19 +1,60 @@
 package com.doodle.backend.domain;
 
-import java.util.UUID;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+import java.time.LocalDateTime;
 
 public class Message {
 
-    private UUID id;
+    @Column(name="id")
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name="body")
+    private String body;
+
+    @Column(name="sent_at")
+    private LocalDateTime sentAt;
+
+    @Column(name="sender_username")
+    private String senderUsername;
+
 
     public Message() {
     }
 
-    public UUID getId() {
+    public int getId() {
         return id;
     }
 
-    public void setId(UUID id) {
+    public void setId(int id) {
         this.id = id;
+    }
+
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public LocalDateTime getSentAt() {
+        return sentAt;
+    }
+
+    public void setSentAt(LocalDateTime sentAt) {
+        this.sentAt = sentAt;
+    }
+
+    public String getSenderUsername() {
+        return senderUsername;
+    }
+
+    public void setSenderUsername(String senderUsername) {
+        this.senderUsername = senderUsername;
     }
 }
