e7582e26ec7280ab730e5bf3f737563a37513d0d
Alexandre Vanier <alexandre.vanier2@gmail.com>
2024-01-31 03:31:54 +0100

    Add dockerfile for web app

--- backend/src/main/java/com/doodle/backend/config/WebSocketConfig.java
+++ backend/src/main/java/com/doodle/backend/config/WebSocketConfig.java
@@ -23,7 +23,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-chat").setAllowedOrigins("http://localhost:3000");
+        registry.addEndpoint("/ws-chat").setAllowedOrigins("http://localhost:3100");
     }
 
     @Override
